e51792cb3e70e359a34319b1dbf020295c4b5a9e
todiez <tobias@diez.cc>
2024-09-29 20:03:04 +0200

    mongodb connection etablished

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 
 //creates an express app and stores it in app
 const app = express();
@@ -20,9 +21,16 @@ app.use((req, res, next) => {
 //grabs all different routes attached to the router and uses them on the app
 app.use("/api/workouts", workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 3000");
-});
 
-process.env;
+//connect to Database, it's async and returns a promise
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests after the connection to the DB is etablished
+    app.listen(process.env.PORT, () => {
+      console.log("connected to data base & listening on port", process.env.PORT );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
