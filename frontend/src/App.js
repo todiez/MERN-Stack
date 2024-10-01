01d850677f0e72f425fe45ba687e78c848f4caaf
todiez <tobias@diez.cc>
2024-10-01 12:37:48 +0200

    css and navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages and components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h2>Test Output</h2>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          {/* div for styling only */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
