7906fb18336b6b1e523a0d9f39e97622a7a109b4
todiez <tobias@diez.cc>
2024-10-01 12:27:52 +0200

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Test Output</h2>
+    </div>
+  );
+}
+
+export default App;
